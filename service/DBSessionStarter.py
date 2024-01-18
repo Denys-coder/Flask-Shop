c2644a3a16d3ad58561989b312b7a830b7c83df0
Denys Lapshyn <denislapshin1@gmail.com>
2024-01-18 17:36:44 +0200

    added Dockerfile and docker-compose.yml (it does not working)

--- service/DBSessionStarter.py
+++ service/DBSessionStarter.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///identifier.sqlite', echo=True)
+engine = create_engine('postgresql://postgres:123@localhost:5432/flask-shop', echo=True)
 Session = sessionmaker(bind=engine)
 db_session = Session()
 Base = declarative_base()
